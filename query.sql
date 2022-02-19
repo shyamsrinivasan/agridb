cf0dc5aa9f2122c70385987cc3ed46378b6350a3
Shyam Srinivasan <shyamsweth@gmail.com>
2022-02-19 22:18:56 +0530

    example create table statement for testdb

--- query.sql
+++ query.sql
@@ -7,6 +7,17 @@ WHERE TABLE_NAME = 'film'*/
 /*SELECT * FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_NAME = 'film'*/
 
-CREATE TABLE `agrid`.`expense` (`column_name` int(11) NULL, `column_2` varchar(55) NOT NULL, 
-PRIMARY KEY (`column_name`), KEY `column_name` (`index_name`), 
-CONSTRAINT `constraint_name` FOREIGN KEY (`column_name`) REFERENCES `table_name` (`column_in_foreign_table`) ON DELETE CASCADE)
+/*CREATE TABLE `agrid`.`expense` (`column_name` int(11) NULL, `column_2` varchar(55) NOT NULL, 
+PRIMARY KEY (`column_name`), KEY `index_name` (`column_name`), 
+CONSTRAINT `constraint_name` FOREIGN KEY (`column_name`) REFERENCES `table_name` (`column_in_foreign_table`) ON DELETE CASCADE)*/
+
+CREATE TABLE `testdb`.`items` (
+  `itemid` INT NOT NULL,
+  `name` VARCHAR(30) NULL,
+  `type` VARCHAR(15) NULL DEFAULT 'undefined',
+  `cost` DECIMAL(10,2) NULL DEFAULT 0.00,
+  `from_date` TIMESTAMP(3) NULL DEFAULT CURRENT_TIMESTAMP,
+  `item_num` INT NOT NULL AUTO_INCREMENT,
+  `to_date` TIMESTAMP(3) NULL DEFAULT NULL,
+  PRIMARY KEY (`itemid`, `item_num`),
+  UNIQUE INDEX `item_num_UNIQUE` (`item_num` ASC) VISIBLE);
