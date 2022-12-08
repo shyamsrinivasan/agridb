0c8e0fb2313231b4d2d06b782bf75f3164dc083c
Shyam Srinivasan <shyamsweth@gmail.com>
2022-12-08 21:48:58 +0530

    working homepage for app with navbar

--- agriapp/admin/views.py
+++ agriapp/admin/views.py
@@ -3,7 +3,22 @@ from . import admin_bp
 
 
 @admin_bp.route('/')
-@admin_bp.route('/index')
+@admin_bp.route('/home')
 def homepage():
     """homepage for agridb"""
     return render_template('index.html')
+
+
+@admin_bp.route('/faq')
+def faq():
+    return render_template('faq.html')
+
+
+@admin_bp.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@admin_bp.route('/contact')
+def contact_us():
+    return render_template('contact_admin.html')
